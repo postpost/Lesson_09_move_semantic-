eb689ea9c467fcbd0dd105e3bdc582e3d6ac7f2a
predttt <timofeevapolina88@gmail.com>
2023-09-19 15:43:34 -0300

    task_2 modified

--- Task_02/Task_02/main.cpp
+++ Task_02/Task_02/main.cpp
@@ -24,20 +24,20 @@ public:
 	//конструктор перемещения (move ctr)
 	big_integer(big_integer&& other) {
 		
-		int n = digits_.size();
+		/*int n = digits_.size();
 		for (int i = n - 1; i >= 0; --i) {
 			digits_.push_back(other.digits_[i]);
 		}
 		digits_ = other.digits_;
-		other.digits_ = nullptr;
+		other.digits_ = nullptr;*/
+		digits_ = std::move(other.digits_);
 	}
 	//перемещающий оператор присваивания (move assignment)
 	big_integer& operator=(big_integer&& other) {
 		if (this == &other)
 			return *this;
-		length = other.length;
-		digits_ = other.digits_;
-		other.digits_ = nullptr;
+		length = std::move(other.length);
+		digits_ = std::move(other.digits_);
 		return *this;
 	}
 
@@ -54,8 +54,8 @@ public:
 
 		//sum
 		for (int i = 0; i < max_length; ++i) {
-			last_a = i < length ? digits_[length - i] - '0' : 0;
-			last_b = i < other.length ? other.digits_[other.length - i] - '0' : 0;
+			last_a = i < length ? digits_[length-1 - i] - '0' : 0;
+			last_b = i < other.length ? other.digits_[other.length-1 - i] - '0' : 0;
 			//sum of two last digits
 			res = char((last_a + last_b + carry) % 10 + '0') + res;
 			//if num > 9 then carry will be 1st digit
@@ -82,31 +82,21 @@ public:
 		return res;
 	}
 
-	////оператор умножения на полудлинное число (слева)
-	//friend big_integer operator*(big_integer& left, big_integer& right) {
-	//	int BASE = 1e9;
-	//	std::vector<int> vc;
-	//	big_integer res;
-	//	vc.resize(left.digits_.size()+ right.digits_.size());
-	//	/*if (vc.size() == 1) {
-	//		res = left.digits_[0] * right.digits_[0];
-	//		return big_integer(res);
-	//	}
-	//	*/
-	//	for (int i = 0; i < left.digits_.size(); ++i) {
-	//		int carry = 0;
-	//		for (int j = 0; j < right.digits_.size(); ++j) {
-	//			vc[i + j] += carry + left.digits_[i] * right.digits_[j];
-	//			carry = vc[i + j] / 10;
-	//			vc[i + j] %= 10;
-	//			}
-	//		vc.push_back(carry);
-	//	}
-	//	res.digits_.resize(vc.size());
-	//	for (int i = 0; i < vc.size(); ++i)
-	//		res.digits_[i] = vc[i];
-	//	return res;
-	//};
+	//оператор умножения на полудлинное число (слева)
+	friend big_integer operator*(const long long& left, big_integer& right) {
+		int carry = 0;
+		int last_digit;
+		std::string res = "";
+		for (int i = right.digits_.size() - 1; i >= 0; --i) {
+			last_digit = int(right.digits_[i] - '0') * left + carry;
+			carry = last_digit / 10;
+			res = char(last_digit % 10 + '0') + res;
+		}
+		while (carry)
+			res = char(carry % 10 + '0') + res;
+		carry /= 10;
+		return res;
+	};
 
 	//оператор умножения двух больших чисел
 	friend big_integer& operator*=(big_integer& left, const big_integer& right) {
@@ -152,7 +142,7 @@ int main() {
 	auto number2 = big_integer("78524");
 	auto result = number1 + number2; 
 	auto num_1 = big_integer("6459134876312645915375645915243562819842157684951435975462712564565467216417897215674812546818645 ");
-	auto num_2 = big_integer("5643269487");
+	auto num_2 = 5643269487;
 	auto mult0 = num_1 * num_2;
 	std::cout << '\n' << result << std::endl; // 193099
 	std::cout << "mult_0: " << mult0 << std::endl;
