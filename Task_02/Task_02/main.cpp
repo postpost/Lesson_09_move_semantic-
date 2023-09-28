0662b5f8b4d920e5b637e96bfe84f181cbb244f0
predttt <timofeevapolina88@gmail.com>
2023-09-28 17:39:13 -0300

    task_02 updated

--- Task_02/Task_02/main.cpp
+++ Task_02/Task_02/main.cpp
@@ -22,18 +22,11 @@ public:
 		//delete[] ptr_;
 	}
 	//конструктор перемещения (move ctr)
-	big_integer(big_integer&& other) {
-		
-		/*int n = digits_.size();
-		for (int i = n - 1; i >= 0; --i) {
-			digits_.push_back(other.digits_[i]);
-		}
-		digits_ = other.digits_;
-		other.digits_ = nullptr;*/
+	big_integer(big_integer&& other) noexcept {
 		digits_ = std::move(other.digits_);
 	}
 	//перемещающий оператор присваивания (move assignment)
-	big_integer& operator=(big_integer&& other) {
+	big_integer& operator=(big_integer&& other) noexcept {
 		if (this == &other)
 			return *this;
 		length = std::move(other.length);
@@ -99,14 +92,19 @@ public:
 	};
 
 	//оператор умножения двух больших чисел
-	friend big_integer& operator*=(big_integer& left, const big_integer& right) {
-		if (NULL(left) || NULL(right))
+	friend big_integer& operator*=(big_integer& left, big_integer& right) {
+		if (left.digits_ == "" || left.digits_ == "0")
+			return right;
+		else if (right.digits_ == "" || right.digits_ == "0")
 			return left;
 		int n = left.digits_.size(), m = right.digits_.size();
 		std::vector<int> vi(n + m, 0);
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < m; ++j) {
-				vi[i + j] += (left.digits_[i]) * (right.digits_[j]);
+				//Здесь (left.digits_[i]) и (right.digits_[j]) являются символами
+				//вычесть '0' из каждого символа, чтобы получить численное значение
+				//иначе это символы ASCII
+				vi[i + j] += (left.digits_[i] - '0') * (right.digits_[j] - '0');
 			}
 		}
 		n += m;
@@ -115,14 +113,15 @@ public:
 			k = t + vi[i];
 			vi[i] = k % 10;
 			t = k / 10;
-			left.digits_[i] = vi[i];
+			left.digits_[i] = vi[i] + '0'; //vi[i] - это int, и вы пытаетесь присвоить его символьной переменной.
 		}
 		for (int i = n - 1; i >= 1 && !vi[i]; ++i)
 			left.digits_.pop_back();
+		left.digits_.shrink_to_fit();
 		return left;
 	}
 
-	friend big_integer operator* (const big_integer& left, const big_integer& right) {
+	friend big_integer operator* (big_integer& left, big_integer& right) {
 		big_integer temp;
 		temp.digits_ = left.digits_;
 		temp *= right;
@@ -142,7 +141,7 @@ int main() {
 	auto number2 = big_integer("78524");
 	auto result = number1 + number2; 
 	auto num_1 = big_integer("6459134876312645915375645915243562819842157684951435975462712564565467216417897215674812546818645 ");
-	auto num_2 = 5643269487;
+	auto num_2 = big_integer("5643269487 ");
 	auto mult0 = num_1 * num_2;
 	std::cout << '\n' << result << std::endl; // 193099
 	std::cout << "mult_0: " << mult0 << std::endl;
